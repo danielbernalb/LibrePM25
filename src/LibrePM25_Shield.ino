f5615c8f4c50df84f288c842d2ac0964bb8d6e4d
Daniel <42117424+danielbernalb@users.noreply.github.com>
2024-02-15 21:17:57 -0500

    Update LibrePM25_Shield.ino

--- src/LibrePM25_Shield.ino
+++ src/LibrePM25_Shield.ino
@@ -66,7 +66,7 @@ void loop()
   if (pms.readUntil(data))
   {
     PM25_value_ori = data.PM_AE_UG_2_5;
-    PM25_value = ((630 * PM25_value_ori) / 1000) + 1.56; // Tercer ajuste a los que salio en Lima y pruebas aqui
+    PM25_value = ((630 * PM25_value_ori) / 1000) + 1.56; // Ajuste resultado de comparaciones con 4 estaciones oficiales en Bogotá y 1 Lima
     Serial.print(F("PMS PM2.5: "));
     Serial.print(PM25_value);
     Serial.println(F(" ug/m3"));
